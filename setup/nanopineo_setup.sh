4a2995f99730cbff7dfc540ffca2bd2c4365a6d5
Juan Miguel Jimeno <jimenojmm@gmail.com>
2017-06-29 16:17:26 +0800

    added curl dev packages

--- setup/nanopineo_setup.sh
+++ setup/nanopineo_setup.sh
@@ -7,4 +7,13 @@ apt-get install dphys-swapfile
 cp dphys-swapfile /etc
 fs_resize
 
+# install curl dev pacakages
+sudo apt-get install -y libcurl4-gnutls-dev
+
+# TODO: Test if these are still necessary
+#sudo apt-get install -y libssh2-1-dev
+#sudo apt-get install -y libssl-dev
+#sudo apt-get install -y groff
+
+
 echo "Done. Please reboot."
